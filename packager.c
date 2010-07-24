48aaaa74f7052e870d43e840a069eeda7729f404
Astrid Smith <Astrid@xrtc.net>
2010-07-24 16:48:51 -0700

    Created ROM packager utility stub

---
+++ packager.c
@@ -0,0 +1,20 @@
+/* Copyright 2010, Astrid Smith
+ * GPL
+ *
+ * A Linux tool to turn a rom dump into an 89g file of rom segments.
+ */
+
+#include <tilp/tifiles.h>
+#include <tilp/macros.h>
+
+
+int main(int argc, char **argv)
+{
+  Ti9xFlash content;
+
+  tifiles_init();
+
+
+
+  return 0;
+}
