dbe094eeae873ff5788b99d8a9c763df963d3a30
Duncan Smith <Duncan@xrtc.net>
2010-09-07 19:12:01 -0700

    IO port framework now works
    A68k is defaulting all my address moves to word size, rather than
    long.  I don't know whether I've expunged all this nonsense yet, but
    I'm trying.
    
    This version executes an infinite loop which writes 'A' to port 00h.
    I've patched in a write-to-console function on port 00h, so this can
    be used as a sort of debug monitor.

---
+++ debug.c
@@ -0,0 +1,14 @@
+/* Debugging routines for 680 project.
+ *
+ * Includes debug output.
+ *
+ * Copyright 2010, Duncan Smith
+ * GPL
+ */
+
+#include <stdio.h>
+
+void char_draw(char c)
+{
+	putchar((short)c);
+}
